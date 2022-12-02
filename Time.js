d3844acff5d020646b702836c6968eb9b0506a5c
zhangweida <zhangweida@winning-tech.com>
2022-12-02 20:56:13 +0800

    time

---
+++ Time.js
@@ -0,0 +1,66 @@
+let chineseDate = getChineseDate()
+class Time {
+    state = {
+        date: new Date(),
+        template: '',
+    }
+    timer = null
+    componentDidMount(callback) {
+        clearInterval(this.timer)
+        this.timer = setInterval(() => {
+            this.state.date = new Date()
+            this.state.template = this.render()
+            callback && callback(this.state.template)
+        }, 1000)
+    }
+    getLocalDate = () => {
+        const { date } = this.state
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+        return `${year}年${month}月${day}日`
+    }
+    getWeek = (day) => {
+        const weekEnum = {
+            0: '星期日',
+            1: '星期一',
+            2: '星期二',
+            3: '星期三',
+            4: '星期四',
+            5: '星期五',
+            6: '星期六',
+        }
+        return weekEnum[day]
+    }
+    getLocaleTime = () => {
+        const { date } = this.state
+        const add0 = (num) => {
+            num = num.toString()
+            return num.length === 1 ? `0${num}` : num
+        }
+        let hours = date.getHours(), minute = date.getMinutes(), seconds = date.getSeconds()
+
+        return `${add0(hours)}:${add0(minute)}:${add0(seconds)}`
+    }
+    render() {
+        const { date } = this.state
+        return (
+            `<div class='time-wrap'>
+                <h2 class='date-week'>
+                    ${this.getLocalDate()}
+                    &nbsp;
+                    &nbsp;
+                    ${this.getWeek(date.getDay())}
+                </h2>
+                <h4>
+                    <span>${chineseDate.g}</span>&nbsp;
+                    <span>${chineseDate.a}</span>&nbsp;
+                    <span>${chineseDate.m}</span>
+                    <span>${chineseDate.d}</span>
+                </h4>
+                <h1 class='time'>${this.getLocaleTime()}</h1>
+            </div>
+            `
+        )
+    }
+} 
---
+++ Weather.js
@@ -0,0 +1,73 @@
+class Weather {
+    state = {
+        positionApi: 'http://www.nmc.cn/rest/position',
+        weatherApi: 'http://www.nmc.cn/rest/weather',
+        data: null
+    }
+    timeStamp = () => new Date().getTime()
+    componentDidMount() {
+        fetch(`${this.state.positionApi}?_=${this.timeStamp()}`)
+            .then(res => res.json())
+            .then(res => {
+                this.getWeather(res.code)
+            })
+    }
+    getWeather = (stationid) => {
+        fetch(`${this.state.weatherApi}?stationid=${stationid}&_=${this.timeStamp()}`)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({ data: res.data.predict })
+            })
+    }
+
+    transfer = (dayWeather, nightWeather) => {
+        if (dayWeather == '9999') return nightWeather
+        else if (dayWeather && nightWeather) {
+            return dayWeather === nightWeather ? dayWeather : `${dayWeather}转${nightWeather}`
+        }
+        return dayWeather || nightWeather
+    }
+    getWind = (wind) => {
+        return wind == '9999' ? null : wind
+    }
+    getTemperature = (dayTe, nightTe) => {
+        if (dayTe == '9999') return nightTe + '°C'
+        else if (nightTe == '9999') return dayTe + '°C'
+        return `${dayTe}°C ~ ${nightTe}°C`
+    }
+    /**
+     * 
+     * @returns 
+    render() {
+        if (this.state.data) {
+            const { data } = this.state
+            const { data: { detail } } = this.state
+            const currentDay = Array.isArray(detail) && detail[0] ? detail[0] : {}
+            const { day, night } = currentDay
+            const [direct, power] = [this.getWind(day.wind.direct), this.getWind(day.wind.power)]
+            const isSplitLine = direct && power
+            return (
+                <div className='weather-wrap'>
+                    <div className='weather'>
+                        <h1>{this.transfer(day.weather.info, night.weather.info)}</h1>
+                    </div>
+                    <div className='temperature'>
+                        {this.getTemperature(day.weather.temperature, night.weather.temperature)}
+                        {isSplitLine && (<span className='split-line'>|</span>)}
+                    </div>
+                    <div className='wind'>
+                        {direct} {power}
+                    </div>
+                    <div className='station'>
+                        <h4>{data.station.province}•{data.station.city}</h4>
+                    </div>
+                    <div className='divider'></div>
+                </div>
+            )
+        }
+
+    }
+     */
+
+
+}
